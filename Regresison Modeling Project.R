0d23a0745765de85f6ff89685cbc442ac9864a35
acesvd <167221044+acesvd@users.noreply.github.com>
2024-04-16 16:38:54 -0400

    Update Regresison Modeling Project.R

--- Regresison Modeling Project.R
+++ Regresison Modeling Project.R
@@ -26,3 +26,63 @@ fin.rat$net_profit_margin = net_income/revenue
 fin.rat$gross_profit_margin = (sales - cost_goods_sold)/revenue
 fin.rat$return_on_assets = net_income/assets
 fin.rat$return_on_equity = net_income/equity
+
+fin.rat[!is.finite(fin.rat)] = 0
+fin.rat[sapply(fin.rat, is.infinite)] = 0
+
+summary(fin.rat[c(3:14)])
+
+#### Dummy Variables ####
+numrow = nrow(data)
+
+region_dummy = matrix(nrow = numrow, ncol = 1)
+
+for(i in 1:numrow){
+  if(sub_region[i] == "Western_Asia"){
+    region_dummy[i,] = 1
+  } else if(sub_region[i] == "Southern_Asia"){
+    region_dummy[i,] = 2
+  } else if(sub_region[i] == "South_eastern_Asia"){
+    region_dummy[i,] = 3
+  } else if(sub_region[i] == "Micronesia"){
+    region_dummy[i,] = 4
+  } else if(sub_region[i] == "Melanesia"){
+    region_dummy[i,] = 5
+  } else if(sub_region[i] == "Eastern_Asia"){
+    region_dummy[i,] = 6
+  } else if(sub_region[i] == "Central_Asia"){
+    region_dummy[i,] = 7
+  } else {
+    region_dummy[i,] = 0
+  }
+}
+
+head(region_dummy)
+colnames(region_dummy) = "region_dummy"
+
+sector_dummy = matrix(nrow = numrow, ncol = 1)
+
+for(i in 1:numrow){
+  if(sector[i] == "com_services"){
+    sector_dummy[i,] = 1
+  } else if(sector[i] == "cons_disc"){
+    sector_dummy[i,] = 2
+  } else if(sector[i] == "cons_staples"){
+    sector_dummy[i,] = 3
+  } else if(sector[i] == "energy"){
+    sector_dummy[i,] = 4
+  } else if(sector[i] == "health_care"){
+    sector_dummy[i,] = 5
+  } else if(sector[i] == "industrials"){
+    sector_dummy[i,] = 6
+  } else if(sector[i] == "inf_tech"){
+    sector_dummy[i,] = 7
+  } else if(sector[i] == "materials"){
+    sector_dummy[i,] = 8
+  } else {
+    sector_dummy[i,] = 0
+  }
+}
+
+head(sector_dummy)
+colnames(sector_dummy) = "sector_dummy"
