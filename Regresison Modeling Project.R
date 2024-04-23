0babc024720b7be8ddf0baf7bf347c2fd0f280ef
acesvd <167221044+acesvd@users.noreply.github.com>
2024-04-23 09:57:22 -0400

    Update Regresison Modeling Project.R

--- Regresison Modeling Project.R
+++ Regresison Modeling Project.R
@@ -1,91 +1,92 @@
 setwd("C:/Users/EKArc/OneDrive/Documents/2024 Spring Classes/Regression Modeling/Data")
-Data=read.csv("Data.csv",header=TRUE,sep=",")
-names(Data)
-attach(Data)
 
-Data=subset(Data,select= -c(X))
-Data=na.omit(Data)
+library(caret)
+library(Rcmdr)
+library(olsrr)
+library(dummy)
+library(fastDummies)
+
+data = read.csv("Data.csv", header = TRUE)
 
 names(data)
 attach(data)
 
+#### Clean Data ####
+apply(is.na(data), 2, mean)
+
+data1 = subset(data)
+
+for(i in 1:nrow(data1)){
+  if(data1$sector[i] == "#N/A"){
+    data1$sector[i] = "Other"
+  }
+}
+
+aggregate(data1$sector, list(data1$sector), length)
+
+# use for financial ratios
+data1 = subset(data1, select = -c(X)) 
+data1 = na.omit(data1)
+
+for(i in 1:nrow(data1)){
+  if(data1$sales[i] < 0){
+    data1$sales[i] = NA
+  }
+  if(data1$assets[i] <= 0){
+    data1$assets[i] = NA
+  }
+  if(data1$cost_goods_sold[i] < 0){
+    data1$cost_goods_sold[i] = NA
+  }
+  if(data1$equity[i] <= 0){
+    data1$equity[i] = NA
+  }
+  if(data1$revenue[i] < 0){
+    data1$revenue[i] = NA
+  }
+  if(data1$inventory[i] <= 0){
+    data1$inventory[i] = NA
+  }
+  #if(data1$working_capital[i] <= 0){
+  #  data1$working_capital[i] = NA
+  #}
+  #if(data1$fixed_assets[i] <= 0){
+  #  data1$fixed_assets[i] = NA
+  #}
+}
+
+data1 = na.omit(data1)
+
+# use for regression
+data2 = subset(data1) 
+apply(is.na(data), 2, mean)
+
+data2 = subset(data2, select = -c(earnings_per_share, net_income))
+
+
 #### Financial Ratios ####
-fin.rat = subset(data, select = c(1,2))
+ratios = subset(data1, select = c(1,2))
 
-fin.rat$inventory_turnover = cost_goods_sold/inventory
-fin.rat$asset_turnover = revenue/sales
-fin.rat$fixed_asset_turnover = revenue/fixed_assets
-fin.rat$working_capital_turnover = revenue/working_capital
-fin.rat$current_ratio = current_assets/current_liabilities
-fin.rat$quick_ratio = (current_assets - inventory)/current_liabilities
-fin.rat$debt_to_equity = (debt_short_term+debt_long_term)/equity
-fin.rat$debt_to_assets = (debt_short_term+debt_long_term)/assets
-fin.rat$financial_leverage = assets/equity
-fin.rat$interest_coverage = ebit/interest_expense
-fin.rat$net_profit_margin = net_income/revenue
-fin.rat$gross_profit_margin = (sales - cost_goods_sold)/revenue
-fin.rat$return_on_assets = net_income/assets
-fin.rat$return_on_equity = net_income/equity
+ratios$inventory_turnover = data1$cost_goods_sold/data1$inventory
+ratios$asset_turnover = data1$revenue/data1$sales
+ratios$fixed_asset_turnover = data1$revenue/data1$fixed_assets
+ratios$working_capital_turnover = data1$revenue/data1$working_capital
+ratios$current_ratio = data1$current_assets/data1$current_liabilities
+ratios$quick_ratio = (data1$current_assets - data1$inventory)/data1$current_liabilities
+ratios$debt_to_equity = (data1$debt_short_term+data1$debt_long_term)/data1$equity
+ratios$debt_to_assets = (data1$debt_short_term+data1$debt_long_term)/data1$assets
+ratios$financial_leverage = data1$assets/data1$equity
+ratios$interest_coverage = data1$ebit/data1$interest_expense
+ratios$net_profit_margin = data1$net_income/data1$revenue
+ratios$gross_profit_margin = (data1$sales - data1$cost_goods_sold)/data1$revenue
+ratios$return_on_assets = data1$net_income/data1$assets
+ratios$return_on_equity = data1$net_income/data1$equity
 
-fin.rat[is.na(fin.rat)] = 0
-fin.rat[sapply(fin.rat, is.infinite)] = 0
-
-summary(fin.rat[c(3:14)])
+summary(ratios[c(3:14)])
 
 #### Dummy Variables ####
-numrow = nrow(data)
+data2 = dummy_cols(data2, select_columns = c("sub_region", "sector"), remove_first_dummy = TRUE)
 
-region_dummy = matrix(nrow = numrow, ncol = 1)
-
-for(i in 1:numrow){
-  if(sub_region[i] == "Western_Asia"){
-    region_dummy[i,] = 1
-  } else if(sub_region[i] == "Southern_Asia"){
-    region_dummy[i,] = 2
-  } else if(sub_region[i] == "South_eastern_Asia"){
-    region_dummy[i,] = 3
-  } else if(sub_region[i] == "Micronesia"){
-    region_dummy[i,] = 4
-  } else if(sub_region[i] == "Melanesia"){
-    region_dummy[i,] = 5
-  } else if(sub_region[i] == "Eastern_Asia"){
-    region_dummy[i,] = 6
-  } else if(sub_region[i] == "Central_Asia"){
-    region_dummy[i,] = 7
-  } else {
-    region_dummy[i,] = 0
-  }
-}
-
-head(region_dummy)
-colnames(region_dummy) = "region_dummy"
-
-sector_dummy = matrix(nrow = numrow, ncol = 1)
-
-for(i in 1:numrow){
-  if(sector[i] == "com_services"){
-    sector_dummy[i,] = 1
-  } else if(sector[i] == "cons_disc"){
-    sector_dummy[i,] = 2
-  } else if(sector[i] == "cons_staples"){
-    sector_dummy[i,] = 3
-  } else if(sector[i] == "energy"){
-    sector_dummy[i,] = 4
-  } else if(sector[i] == "health_care"){
-    sector_dummy[i,] = 5
-  } else if(sector[i] == "industrials"){
-    sector_dummy[i,] = 6
-  } else if(sector[i] == "inf_tech"){
-    sector_dummy[i,] = 7
-  } else if(sector[i] == "materials"){
-    sector_dummy[i,] = 8
-  } else {
-    sector_dummy[i,] = 0
-  }
-}
-
-head(sector_dummy)
-colnames(sector_dummy) = "sector_dummy"
 
 #VIF
 vif_values <- vif(model)
